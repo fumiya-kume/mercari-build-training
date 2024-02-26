abee7f988da30b60f7a05f868dcbd73f6dbd2351
fumiya-kume <fumiya.kume@hotmail.com>
2024-02-26 17:12:16 +0900

    Fix code format with go fmt

--- go/calc_test.go
+++ go/calc_test.go
@@ -1,14 +1,13 @@
 package calc
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestSum(t *testing.T) {
-    got := Sum(1, 2)
-    want := 3
-    if got != want {
-        t.Errorf("Sum with 1,2 == %d, want %d", got, want)
-    }
+	got := Sum(1, 2)
+	want := 3
+	if got != want {
+		t.Errorf("Sum with 1,2 == %d, want %d", got, want)
+	}
 }
-
