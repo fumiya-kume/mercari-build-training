3421863ce282db97b94da085cdb77991cad49dc6
fumiya-kume <fumiya.kume@hotmail.com>
2024-02-26 17:01:01 +0900

    Add example application

---
+++ go/calc_test.go
@@ -0,0 +1,14 @@
+package calc
+
+import (
+    "testing"
+)
+
+func TestSum(t *testing.T) {
+    got := Sum(1, 2)
+    want := 3
+    if got != want {
+        t.Errorf("Sum with 1,2 == %d, want %d", got, want)
+    }
+}
+
